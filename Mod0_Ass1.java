5a49fc5a9bbb3d4a40a6911b67e4e3e920d8d2a5
justinotime <jjnatividad1209@gmail.com>
2022-07-05 13:23:47 -0700

    JDK included

--- Mod0_Ass1.java
+++ Mod0_Ass1.java
@@ -4,69 +4,70 @@ import java.util.Scanner;
 // compute their personal taxes
 public class Mod0_Ass1 {
     public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            // input variables
-            int userInFiling = 0; 
-            int userTaxIncome = 0;
-            double userTaxAmount = 0;
-            
-            // Storing input by keyboard
-            System.out.print("Enter the filing status: ");
-            userInFiling = input.nextInt();
-            System.out.print("Enter the taxable income: ");
-            userTaxIncome = input.nextInt();
+            try (Scanner input = new Scanner(System.in)) {
+                // input variables
+                int userInFiling = 0; 
+                int userTaxIncome = 0;
+                double userTaxAmount = 0;
+                
+                // Storing input by keyboard
+                System.out.print("Enter the filing status: ");
+                userInFiling = input.nextInt();
+                System.out.print("Enter the taxable income: ");
+                userTaxIncome = input.nextInt();
 
-            // Conditionals
-            switch (userInFiling) 
-            {
-                case 0:
-                    if (userTaxIncome <= 8350) {
-                        userTaxAmount += userTaxIncome * 0.10;
-                    }
-                    else {
-                        userTaxAmount += 8350 * 0.10;
-                    }
-
-                    if (userTaxIncome > 8350) {
-                        if (userTaxIncome <= 33950) {
-                            userTaxAmount += (userTaxIncome - 8350) * 0.15;
+                // Conditionals
+                switch (userInFiling) 
+                {
+                    case 0:
+                        if (userTaxIncome <= 8350) {
+                            userTaxAmount += userTaxIncome * 0.10;
                         }
                         else {
-                            userTaxAmount += 25600 * 0.15;
+                            userTaxAmount += 8350 * 0.10;
                         }
-                    }
-                    
-                    if (userTaxIncome > 33950) {
-                        if (userTaxIncome <= 82250) {
-                            userTaxAmount += (userTaxIncome - 33950) * 0.25;
-                        }
-                        else {
-                            userTaxAmount += 48300 * 0.25;
-                        }
-                    }
 
-                    if (userTaxIncome > 82250) {
-                        if (userTaxIncome <= 171550) {
-                            userTaxAmount += (userTaxIncome - 82250) * 0.28;
+                        if (userTaxIncome > 8350) {
+                            if (userTaxIncome <= 33950) {
+                                userTaxAmount += (userTaxIncome - 8350) * 0.15;
+                            }
+                            else {
+                                userTaxAmount += 25600 * 0.15;
+                            }
                         }
-                        else {
-                            userTaxAmount += 89300 * 0.28;
-                        }   
-                    }
+                        
+                        if (userTaxIncome > 33950) {
+                            if (userTaxIncome <= 82250) {
+                                userTaxAmount += (userTaxIncome - 33950) * 0.25;
+                            }
+                            else {
+                                userTaxAmount += 48300 * 0.25;
+                            }
+                        }
 
-                    
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default : System.out.println("Failure...");
-            }
-            System.out.println("You owe " + userTaxAmount);
-        /* 
-        Given 
-        */ 
+                        if (userTaxIncome > 82250) {
+                            if (userTaxIncome <= 171550) {
+                                userTaxAmount += (userTaxIncome - 82250) * 0.28;
+                            }
+                            else {
+                                userTaxAmount += 89300 * 0.28;
+                            }   
+                        }
+
+                        
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default : System.out.println("Failure...");
+                }
+                System.out.println("You owe " + userTaxAmount);
+      /* 
+      Given 
+      */
+            } 
     }
 }
---
+++ Mod0_Ex4.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+// Justin Natividad | CIS 35A
+// This program calculates labor pay rate.
+public class Mod0_Ex4 {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Variables
+        int userWorkHours = 0;
+        int workPayRate = 0;
+        double userpaid = 0.0;
+
+        // Giving input
+        System.out.print("Enter hours: ");
+        userWorkHours = input.nextInt();
+        System.out.print("Enter rate: ");
+        workPayRate = input.nextInt();
+
+        //Computation
+        if (userWorkHours > 40) {
+            userpaid = (40 * workPayRate) + (userWorkHours - 40) * workPayRate * 1.5;
+        }
+        else if (userWorkHours < 40) {
+            userpaid = userWorkHours * workPayRate;
+        }
+
+        System.out.println(userpaid);
+
+
+
+
+
+    }
+}
+/*
+    Enter hours: 55
+    Enter rate: 16
+    1000.0
+*/
