544355db35b121bc487a65466390ea816642686e
justinotime <jjnatividad1209@gmail.com>
2022-07-04 20:41:46 -0700

    lol

--- Mod0_Ass1.java
+++ Mod0_Ass1.java
@@ -9,6 +9,7 @@ public class Mod0_Ass1 {
             int userInFiling = 0; 
             int userTaxIncome = 0;
             double userTaxAmount = 0;
+            
             // Storing input by keyboard
             System.out.print("Enter the filing status: ");
             userInFiling = input.nextInt();
@@ -16,16 +17,54 @@ public class Mod0_Ass1 {
             userTaxIncome = input.nextInt();
 
             // Conditionals
-            if (userInFiling == 0 && userTaxIncome >= 8350) {
-                userTaxAmount = 8350 * 0.10;
-            }
-            else {
-                System.out.print("Failure");
-            }
+            switch (userInFiling) 
+            {
+                case 0:
+                    if (userTaxIncome <= 8350) {
+                        userTaxAmount += userTaxIncome * 0.10;
+                    }
+                    else {
+                        userTaxAmount += 8350 * 0.10;
+                    }
 
-        // Conditionals
+                    if (userTaxIncome > 8350) {
+                        if (userTaxIncome <= 33950) {
+                            userTaxAmount += (userTaxIncome - 8350) * 0.15;
+                        }
+                        else {
+                            userTaxAmount += 25600 * 0.15;
+                        }
+                    }
+                    
+                    if (userTaxIncome > 33950) {
+                        if (userTaxIncome <= 82250) {
+                            userTaxAmount += (userTaxIncome - 33950) * 0.25;
+                        }
+                        else {
+                            userTaxAmount += 48300 * 0.25;
+                        }
+                    }
 
-        // Computation
+                    if (userTaxIncome > 82250) {
+                        if (userTaxIncome <= 171550) {
+                            userTaxAmount += (userTaxIncome - 82250) * 0.28;
+                        }
+                        else {
+                            userTaxAmount += 89300 * 0.28;
+                        }   
+                    }
+
+                    
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default : System.out.println("Failure...");
+            }
+            System.out.println("You owe " + userTaxAmount);
         /* 
         Given 
         */ 
