d75d5be62e52c90eded4f3cf99fb18ef5d06fdc8
justinotime <jjnatividad1209@gmail.com>
2022-07-07 18:05:52 -0700

    Adding onto Homework

---
+++ Mod0_Ex5.java
@@ -0,0 +1,20 @@
+public class Mod0_Ex5 {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Variables
+        int userWorkHours = 0;
+        int workPayRate = 0;
+        double userpaid = 0.0;
+
+        // Giving input
+        System.out.print("Enter hours: ");
+        userWorkHours = input.nextInt();
+        System.out.print("Enter rate: ");
+        workPayRate = input.nextInt();
+        
+        System.out.println(userpaid);
+
+    }
+}
+
