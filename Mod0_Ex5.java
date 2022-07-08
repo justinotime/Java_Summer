0fdc8f1aad55092dae6f0ead40101d7e194c46e0
justinotime <jjnatividad1209@gmail.com>
2022-07-07 19:11:05 -0700

    Hello

--- Mod0_Ex5.java
+++ Mod0_Ex5.java
@@ -1,20 +1,40 @@
+import java.util.Scanner;
+
 public class Mod0_Ex5 {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
         
-        // Variables
-        int userWorkHours = 0;
-        int workPayRate = 0;
-        double userpaid = 0.0;
-
         // Giving input
-        System.out.print("Enter hours: ");
-        userWorkHours = input.nextInt();
-        System.out.print("Enter rate: ");
-        workPayRate = input.nextInt();
+        System.out.print("Enter a string: ");
+        String userString = input.nextLine();
+        System.out.println("Original: " + userString);
+
+        // 1. Trim any white-Spaces
+        String whiteString = userString.replaceAll("\\s","");
+        System.out.println("Trimmed white space: " + whiteString);
+
+        // 2. Show the length of the string
+        System.out.println("The length of original string is " + userString.length());
         
-        System.out.println(userpaid);
+        // 3. Change the string to all caps
+        System.out.println("If your string were all in caps: " + userString.toUpperCase());
+
+        // 4. Print the first character of the string
+        System.out.println("The first character of your string is: " + userString.charAt(0));
+        
+        // 5. Concatenate the string to "Your input string was "
+        System.out.println("Your input string was: " + userString);
+        
 
     }
 }
 
+/*
+Enter a string: Holy moly I love you
+Original: Holy moly I love you
+Trimmed white space: HolymolyIloveyou
+The length of original string is 20
+If your string were all in caps: HOLY MOLY I LOVE YOU
+The first character of your string is: H
+Your input string was Holy moly I love you
+*/
