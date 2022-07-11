415a6154d529f487e696ebe24148d3cf88e71247
justinotime <jjnatividad1209@gmail.com>
2022-07-11 00:29:55 -0700

    new file

---
+++ Exercises/Mod0_Ex8.java
@@ -0,0 +1,21 @@
+package Exercises;
+
+import java.util.Scanner;
+
+// Justin Natividad CIS 35A
+// This code asks for the user string, and determines if it is a palindrome or not 
+public class Mod0_Ex8 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String userString = input.nextLine();
+        
+        System.out.print(userString);
+
+
+
+    }
+}
