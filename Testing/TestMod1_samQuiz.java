9b3b397acd2b64a4242ece1599164eb46ec8c312
justinotime <jjnatividad1209@gmail.com>
2022-07-11 22:31:28 -0700

    added new file

---
+++ Testing/TestMod1_samQuiz.java
@@ -0,0 +1,11 @@
+package Testing;
+
+public class TestMod1_samQuiz {
+    public static void main(String[] args) {
+        int[] x = new int[] {1,2,3};
+        System.out.println(x[0]);   
+
+        
+
+    }
+}
