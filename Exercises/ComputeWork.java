3703b981ef30a7b06aa238341fa26b4e69acbd4c
justinotime <jjnatividad1209@gmail.com>
2022-07-14 10:04:17 -0700

    New work

---
+++ Exercises/ComputeWork.java
@@ -0,0 +1,31 @@
+package Exercises;
+
+
+public class ComputeWork
+{
+    String prompt;
+    int hours;
+    int rate;
+    double total;
+}
+
+public ComputeWork(String prompt, int hours, int rate, double total)
+{
+    this.prompt = prompt;
+    this.hours = hours;
+    this.rate = rate;
+    this.total = total;
+}
+
+// Method 1
+public String getWhat() 
+{
+    return prompt;
+}
+
+// Method 2 
+public int 
+public static void main(String[] args) 
+{
+    
+}
