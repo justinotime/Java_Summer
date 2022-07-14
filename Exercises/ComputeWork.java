45325b773653c301881642cd8485a464386c8fa6
justinotime <jjnatividad1209@gmail.com>
2022-07-14 10:20:35 -0700

    Remodulize Exercise 2 Mod 1

--- Exercises/ComputeWork.java
+++ Exercises/ComputeWork.java
@@ -18,13 +18,28 @@ public ComputeWork(String prompt, int hours, int rate, double total)
 }
 
 // Method 1
-public String getWhat() 
+public void getInput() 
 {
     return prompt;
 }
 
 // Method 2 
-public int 
+public void setTotal(int hours, int rate)
+{
+    if (hours > 40)
+    {
+        total = (40 * rate) + (hours - 40) * rate * 1.5;
+    }
+    else if (hours < 40)
+    {
+        total = hours * rate;
+    }
+
+}
+
+
+
+// Main Method
 public static void main(String[] args) 
 {
     
---
+++ Testing/TestMod1_Ex2.java
@@ -0,0 +1,23 @@
+package Testing;
+
+import java.util.Scanner;
+
+public class TestMod1_Ex2 {
+    public static void main(String[] args) 
+    {
+        getInputs();
+        calculatePay();
+        printPay();
+    }
+
+    public static void getInputs()
+    {
+        Scanner input = new Scanner(System.in);
+        int inHours = 0;
+        int inRate = 0;
+        System.out.print("Enter hours: ");
+
+        System.out.print("Enter rate: ");
+    }
+
+}
